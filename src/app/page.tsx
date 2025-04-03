3cddb27972bc1c3425ec411627580d7d2c9827a0
Stefan Stäx <stefanstax@MacBook-Air.local>
2025-04-03 19:39:09 +0200

    Readme updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Folder, FileIcon, Upload, ChevronRight } from "lucide-react";
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
 
-export default function GoogleDriveClone() {
+export default function HomePage() {
   const [currentFolder, setCurrentFolder] = useState<string | null>(null);
 
   const getCurrentFiles = () => {
